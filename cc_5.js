5150a8ff229760e59f4dbd0fde3938001e2bb02e
Vincent Crooks <crooksv@usf.edu>
2025-09-28 21:49:49 -0400

    employee objects

---
+++ cc_5.js
@@ -0,0 +1,11 @@
+//coding challenge 05
+//Vincent Crooks
+
+const employees = [
+  { name: "Chloe Davis", hourlyRate: 22.5, hoursWorked: 38 },
+  { name: "Vinny Crooks", hourlyRate: 19.0, hoursWorked: 42 },
+  { name: "Anthony Martinez", hourlyRate: 27.75, hoursWorked: 50 },
+  { name: "Mike Thomas", hourlyRate: 16.5, hoursWorked: 40 },
+];
+
+//employee objects
